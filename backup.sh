fccb607facbdc641ec247ebb271c482a056e5b92
Kabir <kabirtomer@gmail.com>
2017-12-17 16:01:07 +0530

    Add support for spaces in filename for backup.sh

--- backup.sh
+++ backup.sh
@@ -1,29 +1,29 @@
 #!/bin/bash
 
 main(){
-
-    IFS=$'\n'
-
+    
+    IFS=$'\n' 
+    
     local list1=$(ls -1 "$1") 
     
     for i in $list1
     do      
-        if [ ! -e "$2/$i" ]  && [ ! -d "$1/$i" ]
+        if [ ! -e "$2"/"$i" ]  && [ ! -d "$1"/"$i" ]
         then
-            cp "$1/$i" "$2"
+            cp "$1"/"$i" "$2"
             echo "$3$i"
-        elif [ ! -e "$2/$i" ]
+        elif [ ! -e "$2"/"$i" ]
         then
-            mkdir "$2/$i"
-            main "$1/$i" "$2/$i" "$i/"
-        elif [ -d "$2/$i" ]
+            mkdir "$2"/"$i"
+            main "$1"/"$i" "$2"/"$i" "$i"/
+        elif [ -d "$2"/"$i" ]
         then
-            main "$1/$i" "$2/$i" "$i/"
+            main "$1"/"$i" "$2"/"$i" "$i"/
         fi
     done
 }
 
-echo "Files copied from $1 to $2"
-main $1 $2 ""
-echo "Files copied from $2 to $1"
-main $2 $1 "" 
+echo "Files copied from $1 to $2: "
+main "$1" "$2" ""
+echo "Files copied from $2 to $1: "
+main "$2" "$1" "" 
